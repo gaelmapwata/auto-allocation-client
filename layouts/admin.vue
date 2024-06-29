fa4625284b4fcb07b1da43678a9d4f95415bea3c
Gedeon Ebamba <gedebamba3@gmail.com>
2024-06-29 21:26:09 +0100

    user disconnection when the application is not used for a certain period of time

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -18,6 +18,28 @@
   </div>
 </template>
 
+<script lang="ts" setup>
+const { signOut } = useAuth()
+
+let idleTimeout: NodeJS.Timeout
+const IDLE_TIME = 5 * 60 * 1000 // 5 minutes in milliseconds
+const EVENTS_TO_LISTEN_FOR_IDLE = ['mousemove', 'keydown', 'click', 'scroll']
+
+function resetTimer () {
+  clearTimeout(idleTimeout)
+  idleTimeout = setTimeout(onUserInactive, IDLE_TIME)
+}
+
+async function onUserInactive () {
+  EVENTS_TO_LISTEN_FOR_IDLE.forEach(event => window.removeEventListener(event, resetTimer))
+  clearTimeout(idleTimeout)
+  await signOut({ callbackUrl: '/login' })
+}
+
+EVENTS_TO_LISTEN_FOR_IDLE.forEach(event => window.addEventListener(event, resetTimer))
+resetTimer()
+</script>
+
 <style lang="scss">
   .admin-layout-container {
     background-color: white;
